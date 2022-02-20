3e97ccb6ea785264fa3bd6347bc27d2cfad035d7
PaulMak <72689234+PaulMak-del@users.noreply.github.com>
2022-02-20 19:56:47 +0300

    Class_1
    Task 11 (create a function that  print a function) and 12 (bot for finding coins at field)

---
+++ Class1_Task11.py
@@ -0,0 +1,59 @@
+def gen(num):
+    print("def f(n):")
+    if num == 0:
+        print("    return 0")
+    else:
+        first = True
+        retStr = "    return "
+        digit_num = 0
+        while (num != 0):
+            if num % 2 == 1:
+                if (first):
+                    retStr += " (n << " + str(digit_num) + ")"
+                    first = False
+                else:
+                    retStr += " + (n << " + str(digit_num) + ")"
+            digit_num += 1
+            num //= 2
+        print(retStr)
+            
+
+def fast_mul_gen(num):
+    print("def f(n):")
+    if num == 0:
+        print("    return 0")
+    else:
+        first = True
+        current_digit = 0
+        retStr = "    return "
+        number_of_digit = len(str(bin(num))) - 2
+        print("    x1 = n")
+        for i in range(1, number_of_digit):
+            print("    x" + str(2**i), "= x" + str(2**(i-1)), "+ x" + str(2**(i-1)))
+        while (num != 0):
+            if num % 2 == 1:
+                if (first):
+                    retStr += "x" + str(2**current_digit)
+                    first = False
+                else:
+                    retStr += " + x" + str(2**current_digit)
+            current_digit += 1
+            num //= 2
+        
+        print(retStr)
+
+def f(n):
+    x1 = n
+    x2 = x1 + x1
+    x4 = x2 + x2
+    x8 = x4 + x4
+    x16 = x8 + x8
+    x32 = x16 + x16
+    return x1 + x4 + x16 + x32
+
+x = 53
+fast_mul_gen(x)
+for i in range(100):
+    if f(i) != i * x:
+        raise Exception("ERROR, input:", i)
+    
---
+++ user_bot.py
@@ -0,0 +1,96 @@
+﻿
+def script(check, x, y):
+    if check("gold", x, y):
+        return "take"
+    field = []
+    width = 30
+    height = 30
+
+    nearest_distance = 999
+    nearest_x = 0
+    nearest_y = 0
+    for i in range(height):
+        field.append([0] * width)
+    field[y][x] = 1
+
+    # ---Mark cells---#
+    for x_in in range(width):
+        for y_in in range(height):
+            if check("gold", x_in, y_in) > 0:
+                field[y_in][x_in] = -7
+                if nearest_distance > (x_in - x) ** 2 + (y_in - y) ** 2:
+                    nearest_x = x_in
+                    nearest_y = y_in
+                    nearest_distance = (x_in - x) ** 2 + (y_in - y) ** 2
+            elif check("wall", x_in, y_in) > 0:
+                field[y_in][x_in] = -9
+
+    coin_is_not_find = True
+    while coin_is_not_find:
+        if field[nearest_y][nearest_x + 1] > 0 or field[nearest_y][nearest_x - 1] > 0 or field[nearest_y + 1][nearest_x] > 0 or field[nearest_y - 1][nearest_x] > 0:
+            coin_is_not_find = False
+        for x_in in range(width):
+            for y_in in range(height):
+                if field[y_in][x_in] > 0:
+                    if x_in < width - 1:
+                        if field[y_in][x_in + 1] == 0:
+                            field[y_in][x_in + 1] = field[y_in][x_in] + 1
+                    if x_in > 0:
+                        if field[y_in][x_in - 1] == 0:
+                            field[y_in][x_in - 1] = field[y_in][x_in] + 1
+                    if y_in < height - 1:
+                        if field[y_in + 1][x_in] == 0:
+                            field[y_in + 1][x_in] = field[y_in][x_in] + 1
+                    if y > 0:
+                        if field[y_in - 1][x_in] == 0:
+                            field[y_in - 1][x_in] = field[y_in][x_in] + 1
+
+    # ---Mark cells---#
+
+    # ---Find the path---#
+    current_x = nearest_x
+    current_y = nearest_y
+
+    if check("gold", x + 1, y) > 0:
+        return "right"
+    if check("gold", x - 1, y) > 0:
+        return "left"
+    if check("gold", x, y + 1) > 0:
+        return "down"
+    if check("gold", x, y - 1) > 0:
+        return "up"
+
+    count = 999
+    while count > 2:
+        if 0 < field[current_y][current_x + 1] < count and current_x < width - 1:
+            count = field[current_y][current_x + 1]
+            if count != 1:
+                current_x = current_x + 1
+        elif 0 < field[current_y][current_x - 1] < count and current_x > 0:
+            count = field[current_y][current_x - 1]
+            if count != 1:
+                current_x = current_x - 1
+        elif 0 < field[current_y + 1][current_x] < count and current_y < height - 1:
+            count = field[current_y + 1][current_x]
+            if count != 1:
+                current_y = current_y + 1
+        elif 0 < field[current_y - 1][current_x] < count and current_y > 0:
+            count = field[current_y - 1][current_x]
+            if count != 1:
+                current_y = current_y - 1
+    # ---Find the path---#
+
+    # ---Do the move---#
+    if x < current_x:
+        return "right"
+    if x > current_x:
+        return "left"
+    if y < current_y:
+        return "down"
+    if y > current_y:
+        return "up"
+
+    # ---Do the move---#
+
+    print('Something wrong')
+    return "pass"
